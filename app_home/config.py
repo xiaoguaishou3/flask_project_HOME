5eedf331228b0039b4b023bf3b004ed7762c8a84
Emily <747759106@qq.com>
2020-12-11 16:49:03 +0800

    20201210有关session 进度(待整理)

--- app_home/config.py
+++ app_home/config.py
@@ -5,7 +5,8 @@ import redis
 class Config(object):
     """配置信息"""
 
-    # TODO: SECRET_KEY = 'ASDAXCWE5ERTFG%%DAS34'
+    # 加盐 可以自定义（待理解整理笔记）
+    SECRET_KEY = 'ASDAXCWE5ERTFG%%DAS34'
     USERNAME = 'root'
     PASSWORD = 'root'
     HOSTNAME = '127.0.0.1'
@@ -17,8 +18,14 @@ class Config(object):
     REDIS_PORT = 6379
 
     # flask-session
-    # TODO: 将 session 保存在redis中
-    # '''待补充'''
+    #将 session 保存在redis中 会话接口类型
+    SESSION_TYPE = 'redis'
+    # 创建一个redis实例
+    SESSION_REDIS = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
+    # 是否对会话进行签名
+    SESSION_USE_SIGNER = True
+    # 存活时间
+    PERMANENT_SESSION_LIFETIME = 86400
 
     # 数据库
     DB_URL = 'mysql+pymysql://{}:{}@{}:{}/{}'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATEBASE)
